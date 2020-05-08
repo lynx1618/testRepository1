ca4c95f63d91f5abc9c3067a4577b365df55e9a9
lynx1618 <lynx1618@163.com>
2020-05-08 10:56:59 +0800

    gai6

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void aaa(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
